ed60535b1d74a7549837d04675d296958c3582b6
Bruno Martins Peçanha <brunotkf@hotmail.com>
2021-07-22 01:44:15 -0300

    🚑️ Remove diretorio duplicado

--- pecanha.Domain/Entity/Scene.cs
+++
@@ -1,8 +0,0 @@
-﻿using Pecanha.Domain.Enum;
-
-namespace Pecanha.Domain.Entity {
-    public class Scene : To {
-        public string Nome { get; set; }
-        public StateEnum Estado { get; set; }
-    }
-}
