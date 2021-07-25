7721e03823251b987c77ccbe6e742861f3403036
Bruno Martins Peçanha <brunotkf@hotmail.com>
2021-07-25 19:07:54 -0300

    🚧 Adiciona classe de extenção para registrar a ioc dos serviços; 🚧 Adiciona configuração das classes;

---
+++ Pecanha.Repository/EntityConfig/RecordHistoryConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Pecanha.Domain.Entity;
+
+namespace Pecanha.Repository.EntityConfig {
+    public class RecordHistoryConfiguration : IEntityTypeConfiguration<RecordHistory> {
+
+        public void Configure(EntityTypeBuilder<RecordHistory> builder) {
+
+            builder
+             .ToTable("RecordHistory")
+             .HasKey(x => x.Id);
+
+            builder
+            .Property(c => c.RegisteringDate)
+            .HasColumnName("RegisteringDate")
+            .IsRequired();
+
+            builder
+            .Property(c => c.LastUpdate)
+            .HasColumnName("LastUpdate");
+
+            builder
+           .Property(c => c.PreviousState)
+           .HasColumnName("PreviousState")
+           .HasConversion<int>();
+
+            builder
+           .Property(c => c.ActualState)
+           .HasColumnName("ActualState")
+           .HasConversion<int>();
+
+            //Relacionamento com a cena
+            builder
+                .HasOne(x => x.Scene)
+                .WithMany(x => x.RecordHistory)
+                .HasForeignKey(x => x.SceneId)
+                .IsRequired();
+             
+        }
+    }
+}
---
+++ Pecanha.Repository/EntityConfig/SceneConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Pecanha.Domain.Entity;
+
+namespace Pecanha.Repository.EntityConfig {
+    public class SceneConfiguration : IEntityTypeConfiguration<Scene> {
+
+        public void Configure(EntityTypeBuilder<Scene> builder) {
+
+            builder
+             .ToTable("Scene")
+             .HasKey(x => x.Id);
+
+            builder
+            .Property(c => c.RegisteringDate)
+            .HasColumnName("RegisteringDate")
+            .IsRequired();
+
+            builder
+            .Property(c => c.LastUpdate)
+            .HasColumnName("LastUpdate");
+
+            builder
+            .Property(c => c.Name)
+            .HasColumnName("Name");
+
+            builder
+           .Property(c => c.State)
+           .HasColumnName("State")
+           .HasConversion<int>();
+        }
+    }
+}
