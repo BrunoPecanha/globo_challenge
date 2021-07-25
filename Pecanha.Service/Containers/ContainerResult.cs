fcdfadced90723723387bd1a6823e2074f2b26fa
Bruno Martins Peçanha <brunotkf@hotmail.com>
2021-07-25 19:08:19 -0300

    🚧 Adiciona container de resposta das controllers;

---
+++ Pecanha.Service/Containers/ContainerResult.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace Pecanha.Service.Containers {
+    public class ContainerResult {
+        /// <summary>
+        /// Id da empresa.
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public int Id { get; set; }
+        /// <summary>
+        /// Objeto de erro para retorno.
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public bool Valid { get; set; }
+        /// <summary>
+        /// Messagem Padrão de saída
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string Message { get; set; }
+        /// <summary>
+        /// Objeto de saída
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public object Log { get; set; }
+
+    }
+}
