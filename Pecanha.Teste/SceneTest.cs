7071957455ad278f0e51db44e73c55d716a53802
Bruno Martins Peçanha <brunotkf@hotmail.com>
2021-07-27 10:23:32 -0300

    🚧 Adiciona projeto de teste unitários;

---
+++ Pecanha.Teste/SceneTest.cs
@@ -0,0 +1,11 @@
+using System;
+using Xunit;
+
+namespace Pecanha.Teste {
+    public class SceneTest {
+        [Fact(Skip = "Ainda bolando teste")]
+        [Trait("Criar", "")]
+        public void Add_No_Name_Scene() {          
+        }
+    }
+}
