37355ab5707bbfd4cde9842beaba1fa6b040bdbc
Bruno Martins Peçanha <brunotkf@hotmail.com>
2021-07-27 21:06:11 -0300

    ♻️ Altera local dos métodos de regras para validação de testes; 🚧 Adiciona enum para tratamento de regras;

--- Pecanha.Domain/Entity/Scene.cs
+++ Pecanha.Domain/Entity/Scene.cs
@@ -1,4 +1,5 @@
-﻿using Pecanha.Domain.Enum;
+﻿using Pecanha.Domain.Commands;
+using Pecanha.Domain.Enum;
 using System;
 using System.Collections.Generic;
 
@@ -6,18 +7,41 @@ namespace Pecanha.Domain.Entity {
     public class Scene : To {
         public string Name { get; private set; }
         public StateEnum State { get; private set; }
-        public DateTime OperationHour { get; set; }
+        public DateTime OperationHour { get; private set; }
         public ICollection<RecordHistory> RecordHistories { get; set; }
+        public ErroEnum Erro { get; private set; }
 
         public Scene(string name) {
             Name = name;
             State = StateEnum.Pendente;
             OperationHour = DateTime.Now;
-        }      
+            Erro = ErroEnum.NoError;
+        }
 
-        public void UpdateState(StateEnum nextState, DateTime opHour) {
-            this.State = nextState;
-            this.OperationHour = opHour;
+        public void UpdateState(SceneUpdateCommand sceneCommand) {
+
+            //2. Não deve ser permitido inserir uma operação de alteração de estado no futuro.
+            if (sceneCommand.OperationHour > DateTime.Now && !this.IsUndoOperation(this.State, sceneCommand.NextState)) {
+                this.Erro = ErroEnum.FutureAlterNotAllowed;
+            }                
+            //3. Não deve ser permitido desfazer uma operação de alteração de estado que foi realizada há mais de 5 minutos. (opcional)
+            else if (sceneCommand.OperationHour.Subtract(this.OperationHour).TotalMinutes > 5 && this.IsUndoOperation(this.State, sceneCommand.NextState)) {
+                this.Erro = ErroEnum.CantUndoOperation;
+            }                
+
+            if (this.State == StateEnum.Pendente && sceneCommand.NextState == StateEnum.Gravada) {
+                this.Erro = ErroEnum.OpNotAllowed;
+            } else if (this.State == StateEnum.Pendurada && sceneCommand.NextState != StateEnum.Preparada) {
+                this.Erro = ErroEnum.OpNotAllowed;
+            } else if (this.State == StateEnum.Gravada && sceneCommand.NextState != StateEnum.Preparada) {
+                this.Erro = ErroEnum.OpNotAllowed;
+            }        
+
+            this.State = sceneCommand.NextState;
+            this.OperationHour = sceneCommand.OperationHour;
+        }
+        private bool IsUndoOperation(StateEnum actual, StateEnum next) {
+            return actual != StateEnum.Pendente && (next == StateEnum.Pendente || next == StateEnum.Preparada);
         }
     }
 }
---
+++ Pecanha.Domain/Enum/ErroEnum.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace Pecanha.Domain.Enum {
+    public enum ErroEnum {
+        [Description("No Error")]
+        NoError = 0,
+        [Description("Future Alter Not Allowed")]
+        FutureAlterNotAllowed = 1,
+        [Description("Operation Not Allowed")]
+        OpNotAllowed = 2,
+        [Description("Can't Undo Operation")]
+        CantUndoOperation = 3        
+    }
+}
